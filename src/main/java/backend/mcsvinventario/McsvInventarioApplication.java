9db1a182338691f668de33fa4beaa7711072cc66
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-01-19 13:45:11 -0500

    se configuro micro para inventario

---
+++ src/main/java/backend/mcsvinventario/McsvInventarioApplication.java
@@ -0,0 +1,13 @@
+package backend.mcsvinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvInventarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsvInventarioApplication.class, args);
+    }
+
+}
