930fcc9999d28daf20ae3119560fe6dab54e53b6
Victor Orbegozo <victor.orbegozo@codingcrew.la>
2025-02-06 19:14:42 -0500

    se agrego configuracion para log

--- src/main/java/backend/mcsvinventario/advice/ExceptionAdvice.java
+++ src/main/java/backend/mcsvinventario/advice/ExceptionAdvice.java
@@ -2,11 +2,13 @@ package backend.mcsvinventario.advice;
 
 import backend.mcsvinventario.exceptions.MovimientoException;
 import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ExceptionAdvice {
 
@@ -15,14 +17,17 @@ public class ExceptionAdvice {
         if (e.getMessage().equals(MovimientoException.MOVIMIENTO_CANTIDAD_INVALIDA) ||
                 e.getMessage().equals(MovimientoException.MOVIMIENTO_TIPO_INVALIDO) ||
                 e.getMessage().equals(MovimientoException.MOVIMIENTO_PRODUCTO_INVALIDO)) {
+            log.error(e.getMessage(), e);
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         } else {
+            log.error(e.getMessage(), e);
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @ExceptionHandler(FeignException.class)
     public ResponseEntity<String> handleFeignException(FeignException e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
